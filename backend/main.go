3cd8bd0b8b4edec353ec05e752e1efdc5b3c87f2
mmNalaka <nalaka.manathunga@gmail.com>
2022-02-04 23:56:38 +0100

    chore: setup chi

--- backend/main.go
+++ backend/main.go
@@ -1,5 +1,17 @@
 package main
 
+import (
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
 func main() {
-	println("Hello, world!")
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello World!"))
+	})
+	http.ListenAndServe(":3000", r)
 }
