2608195b4b5fd7d0bbdbc37e8a0dc171a1133c98
mmNalaka <nalaka.manathunga@gmail.com>
2022-02-06 23:27:21 +0100

    wip: adds basic routes

--- backend/main.go
+++ backend/main.go
@@ -4,14 +4,13 @@ import (
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
-	"github.com/go-chi/chi/v5/middleware"
+	"github.com/mmnalka/go-short-url/pkg/routes"
 )
 
 func main() {
 	r := chi.NewRouter()
-	r.Use(middleware.Logger)
-	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Hello World!"))
-	})
+
+	routes.PublicRoutes(r)
+
 	http.ListenAndServe(":3000", r)
 }
