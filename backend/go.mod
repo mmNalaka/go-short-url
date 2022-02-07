eeb6225940c6dc66eebcc8b89a76a94f83cda06c
mmNalaka <nalaka.manathunga@gmail.com>
2022-02-08 00:12:40 +0100

    feat: setup middleware

--- backend/go.mod
+++ backend/go.mod
@@ -3,3 +3,5 @@ module github.com/mmnalka/go-short-url
 go 1.17
 
 require github.com/go-chi/chi/v5 v5.0.7
+
+require github.com/go-chi/cors v1.2.0
--- backend/main.go
+++ backend/main.go
@@ -4,13 +4,22 @@ import (
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/mmnalka/go-short-url/pkg/middlewares"
 	"github.com/mmnalka/go-short-url/pkg/routes"
 )
 
 func main() {
 	r := chi.NewRouter()
 
+	// Middlewares
+	r.Use(middleware.RequestID)
+	r.Use(middleware.RealIP)
+	r.Use(middleware.Logger)
+	r.Use(middlewares.CORSConfig())
+
+	// Routes
 	routes.PublicRoutes(r)
 
-	http.ListenAndServe(":3000", r)
+	http.ListenAndServe(":4000", r)
 }
